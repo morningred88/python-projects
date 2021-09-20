3347e0582cc5d9e2d297faf1e8c9b74fd5407388
Lily Ma <xma50089@gmail.com>
2021-09-19 23:27:30 -0500

    Added inheritance for rectangle class

--- Geometry_Game/geometry_game_v1.py
+++ Geometry_Game/geometry_game_v1.py
@@ -1,33 +1,5 @@
-class Point:
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def falls_in_rectangle(self, rectangle):
-        if rectangle.point1.x < self.x < rectangle.point2.x \
-                and rectangle.point1.y < self.y < rectangle.point2.y:
-            return True
-        else:
-            return False
-
-    def distance_from_point(self, x, y):
-        return ((self.x - x) ** 2 + (self.y - y) ** 2) ** 0.5
-
-
-class Rectangle:
-
-    def __init__(self, point1, point2):
-        self.point1 = point1
-        self.point2 = point2
-
-    def area(self):
-        width = self.point2.x - self.point1.x
-        height = self.point2.y - self.point1.y
-        area = width * height
-        return area
-
 from random import randint
+import turtle
 
 
 class Point:
@@ -59,11 +31,32 @@ class Rectangle:
         area = width * height
         return area
 
+
+# Child class of Rectangle class
+class GuiRectangle(Rectangle):
+
+    def draw(self, canvas):
+        # Go to a certain coordinate
+        canvas.penup()
+        canvas.goto(self.point1.x, self.point1.y)
+
+        canvas.pendown()
+        canvas.forward(self.point2.x - self.point1.x)
+        # left 90 degrees left
+        canvas.left(90)
+        canvas.forward(self.point2.y - self.point1.y)
+        canvas.left(90)
+        canvas.forward(self.point2.x - self.point1.x)
+        canvas.left(90)
+        canvas.forward(self.point2.y - self.point1.y)
+
+        # This method keep the turtle screen stay
+        turtle.done()
+        
 
 # Create rectangle object
-rectangle = Rectangle(
-    Point(randint(0, 9), randint(0, 9)),
-    Point(randint(10, 19), randint(10, 19)))
+rectangle = Rectangle(Point(randint(0, 400), randint(0, 400)),
+    Point(randint(10, 400), randint(10, 400)))
 
 # Print rectangle coordinates
 print(f"Rectangle Coordinates: ({rectangle.point1.x}, {rectangle.point1.y}) \
