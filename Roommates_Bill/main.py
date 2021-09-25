9355cf6bd0586fbed2a2e64777cd51b62209cead
Lily Ma <xma50089@gmail.com>
2021-09-24 23:54:11 -0500

    Added bill class

---
+++ Roommates_Bill/main.py
@@ -0,0 +1,7 @@
+class Bill:
+    """
+    Object that contains data about a bill, such as total amount and period of the bill
+    """
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
