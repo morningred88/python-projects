52809d2f46c78a3bc104186baadcad0afa7b631f
Lily Ma <xma50089@gmail.com>
2021-09-25 16:24:53 -0500

    Ref #2, added PDF report class

--- Roommates_Bill/main.py
+++ Roommates_Bill/main.py
@@ -2,16 +2,36 @@ class Bill:
     """
     Object that contains data about a bill, such as total amount and period of the bill
     """
+
     def __init__(self, amount, period):
         self.amount = amount
         self.period = period
+
 
 class Roommates:
     """
     Creates a roommate who lives in the apartment and pays a share of the bill
     """
+
     def __init__(self, name, days_in_house):
         self.name = name
         self.days_in_house = days_in_house
 
     def pays(self, bill):
+        pass
+
+
+class PdfReport:
+    """
+    Create a PDF file that contains data about the roommate, such as their names, their due amount and the period of the bill.
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, roommate1, roommate2, bill):
+        pass
+
+
+
+
