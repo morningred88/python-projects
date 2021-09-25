84808ec8925aafdd80f7ca39bb1824e1c3a507b8
Lily Ma <xma50089@gmail.com>
2021-09-24 23:58:58 -0500

    Ref #2, added room mate class

--- Roommates_Bill/main.py
+++ Roommates_Bill/main.py
@@ -5,3 +5,13 @@ class Bill:
     def __init__(self, amount, period):
         self.amount = amount
         self.period = period
+
+class Roommates:
+    """
+    Creates a roommate who lives in the apartment and pays a share of the bill
+    """
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill):
