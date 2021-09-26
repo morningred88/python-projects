d9e0739d92b348f45c9cbbb0e7834391adf5df49
Lily Ma <xma50089@gmail.com>
2021-09-25 21:23:12 -0500

    Ref #2, generating pdf method added

--- Roommates_Bill/main.py
+++ Roommates_Bill/main.py
@@ -1,3 +1,6 @@
+from fpdf import FPDF
+
+
 class Bill:
     """
     Object that contains data about a bill, such as total amount and period of the bill
@@ -18,7 +21,7 @@ class Roommate:
         self.days_in_house = days_in_house
 
     def pays(self, bill, roommate2):
-        weight = self.days_in_house/(self.days_in_house + roommate2.days_in_house )
+        weight = self.days_in_house / (self.days_in_house + roommate2.days_in_house)
         return bill.amount * weight
 
 
@@ -31,13 +34,34 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, roommate1, roommate2, bill):
-        pass
+        pdf = FPDF(orientation='p', unit='pt', format='A4')
+        pdf.add_page()
+
+        # Insert title
+        # Style is bold
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt='Flatmates Bill', border=1, align="C", ln=1)
+
+        # Insert period
+        pdf.cell(w=100, h=40, txt='Period:', border=1)
+        pdf.cell(w=200, h=40, txt=bill.period, border=1, ln=1)
+
+        # Insert name and due amount of the first flatmate
+        pdf.cell(w=100, h=40, txt=roommate1.name, border=1)
+        pdf.cell(w=200, h=40, txt=str(roommate1.pays(bill, roommate2)), border=1, ln=1)
+
+        # Insert name and due amount of the second flatmate
+        pdf.cell(w=100, h=40, txt=roommate2.name, border=1)
+        pdf.cell(w=200, h=40, txt=str(roommate2.pays(bill, roommate1)), border=1, ln=1)
+
+        pdf.output(self.filename)
 
 
-bill = Bill(120, "March 2021")
+bill = Bill(120, "September 2021")
 john = Roommate("John", 20)
 marry = Roommate("Mary", 25)
-print(john.pays(bill, marry))
-print(marry.pays(bill, john))
+print(f'John pays {john.pays(bill, marry)}')
+print(f'Marry pays {marry.pays(bill, john)}')
 
-
+pdf_report = PdfReport("Bill.pdf")
+pdf_report.generate(john, marry, bill)
