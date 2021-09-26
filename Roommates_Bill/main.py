3b95d0718de9a3195fbd7e499b39f8976f2c802f
Lily Ma <xma50089@gmail.com>
2021-09-26 02:43:37 -0500

    Ref #2, added command line user interface

--- Roommates_Bill/main.py
+++ Roommates_Bill/main.py
@@ -66,12 +66,17 @@ class PdfReport:
         # Automatically view the PDF file
         webbrowser.open(self.filename)
 
+amount = float(input ("Hey user, enter the bill amount $ "))
+period = input ("What is the bill period? E.g. December 2020: ")
+name1 = input ("What is your name? ")
+days_in_house1 = int(input (f"How many days did {name1} stay in the house during the bill period? "))
+name2 = input ("What is the name of the other roommate? ")
+days_in_house2 = int(input (f"How many days did {name2} stay in the house during the bill period? "))
+bill = Bill(amount, period)
+roommate1 = Roommate(name1, days_in_house1)
+roommate2 = Roommate(name2, days_in_house2)
+print(f'{roommate1.name} pays {roommate1.pays(bill, roommate2)}')
+print(f'{roommate2.name} pays {roommate2.pays(bill, roommate1)}')
 
-bill = Bill(120, "September 2021")
-john = Roommate("John", 20)
-marry = Roommate("Mary", 25)
-print(f'John pays {john.pays(bill, marry)}')
-print(f'Marry pays {marry.pays(bill, john)}')
-
-pdf_report = PdfReport("Bill.pdf")
-pdf_report.generate(john, marry, bill)
+pdf_report = PdfReport(f"{bill.period}.pdf")
+pdf_report.generate(roommate1, roommate2, bill)
